4e442b6fc963d46950c566782d908a93c8afba2e
DilettaZecchinetti <diletta.zecchinetti@gmail.com>
2025-06-05 11:52:17 +0100

    connect frontend to backend with env variable

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,166 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000";
+// const API_BASE = "http://localhost:5000";
+const API_BASE = process.env.REACT_APP_API_BASE_URL;
+
+// export const fetchEvents = async (keyword = "music", city = "") => {
+//   try {
+//     const params = {};
+//     if (keyword) params.keyword = keyword;
+//     if (city) params.city = city;
+
+//     const response = await axios.get(`${API_BASE}/api/events`, { params });
+//     return response.data;
+//   } catch (err) {
+//     console.error("Error fetching events:", err);
+//     throw err;
+//   }
+// };
+
+// export const fetchEventsById = async (id) => {
+//   try {
+//     const response = await axios.get(`${API_BASE}/api/events/${id}`);
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error fetching events by ID:", error);
+//   }
+// };
+
+// export const registerUser = async (name, email, password, role) => {
+//   try {
+//     const response = await axios.post(`${API_BASE}/api/auth/signup`, {
+//       name,
+//       email,
+//       password,
+//       role,
+//     });
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error registering user:", error);
+//     throw error;
+//   }
+// };
+
+// export const loginUser = async (email, password) => {
+//   try {
+//     const response = await axios.post(`${API_BASE}/api/auth/login`, {
+//       email,
+//       password,
+//     });
+//     const { token, user } = response.data;
+
+//     localStorage.setItem("token", token);
+
+//     return { user, token };
+//   } catch (error) {
+//     console.error("Login failed:", error);
+//     throw error;
+//   }
+// };
+
+// export const fetchCurrentUser = async () => {
+//   const token = localStorage.getItem("token");
+//   if (!token) throw new Error("No token found");
+
+//   const response = await axios.get(`${API_BASE}/api/auth/me`, {
+//     headers: {
+//       Authorization: `Bearer ${token}`,
+//     },
+//   });
+//   return response.data;
+// };
+
+// export const signupForEvent = async (eventId, token) => {
+//   const response = await axios.post(
+//     `${API_BASE}/api/events/${eventId}/signup`,
+//     {},
+//     {
+//       headers: { Authorization: `Bearer ${token}` },
+//     }
+//   );
+//   return response.data;
+// };
+
+// export const addEventToCalendar = (eventId, token) => {
+//   return axios.post(
+//     `${API_BASE}/api/calendar/add-event`,
+//     { eventId },
+//     {
+//       headers: { Authorization: `Bearer ${token}` },
+//       withCredentials: true,
+//     }
+//   );
+// };
+
+// export const createEvent = async (data, token) => {
+//   return axios
+//     .post("/api/events", data, {
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     })
+//     .then((res) => res.data);
+// };
+
+// export const updateEvent = async (id, data, token) => {
+//   return axios
+//     .put(`/api/events/${id}`, data, {
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     })
+//     .then((res) => res.data);
+// };
+
+// export const deleteEvent = async (id, token) => {
+//   return axios
+//     .delete(`/api/events/${id}`, {
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     })
+//     .then((res) => res.data);
+// };
+
+// export const fetchManualEventById = async (id) => {
+//   try {
+//     const response = await axios.get(`${API_BASE}/api/events/manual/${id}`);
+//     return response.data;
+//   } catch (error) {
+//     console.error("Failed to fetch manual event:", error);
+//     throw error;
+//   }
+// };
+
+// export const fetchTicketmasterEventById = async (id) => {
+//   try {
+//     const response = await axios.get(`${API_BASE_URL}/ticketmaster/${id}`);
+//     return response.data;
+//   } catch (error) {
+//     console.error("Failed to fetch Ticketmaster event:", error);
+//     throw error;
+//   }
+// };
+
+// export const getManualEvents = async () => {
+//   const API_BASE_URL = "http://localhost:5000/api/events";
+//   const response = await axios.get(`${API_BASE_URL}/manual`);
+//   return response.data;
+// };
+
+// export const fetchFromTicketmaster = async (externalId) => {
+//   const API_KEY = process.env.TICKETMASTER_API_KEY;
+
+//   try {
+//     const res = await axios.get(
+//       `https://app.ticketmaster.com/discovery/v2/events/${externalId}.json?apikey=${API_KEY}`
+//     );
+//     return res.data;
+//   } catch (error) {
+//     console.error("Failed to fetch from Ticketmaster", error.message);
+//     return null;
+//   }
+// };
 
 export const fetchEvents = async (keyword = "music", city = "") => {
   try {
@@ -82,7 +242,7 @@ export const signupForEvent = async (eventId, token) => {
 
 export const addEventToCalendar = (eventId, token) => {
   return axios.post(
-    "http://localhost:5000/api/calendar/add-event",
+    `${API_BASE}/api/calendar/add-event`,
     { eventId },
     {
       headers: { Authorization: `Bearer ${token}` },
@@ -93,7 +253,7 @@ export const addEventToCalendar = (eventId, token) => {
 
 export const createEvent = async (data, token) => {
   return axios
-    .post("/api/events", data, {
+    .post(`${API_BASE}/api/events`, data, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -103,7 +263,7 @@ export const createEvent = async (data, token) => {
 
 export const updateEvent = async (id, data, token) => {
   return axios
-    .put(`/api/events/${id}`, data, {
+    .put(`${API_BASE}/api/events/${id}`, data, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -113,7 +273,7 @@ export const updateEvent = async (id, data, token) => {
 
 export const deleteEvent = async (id, token) => {
   return axios
-    .delete(`/api/events/${id}`, {
+    .delete(`${API_BASE}/api/events/${id}`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -123,7 +283,7 @@ export const deleteEvent = async (id, token) => {
 
 export const fetchManualEventById = async (id) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/manual/${id}`);
+    const response = await axios.get(`${API_BASE}/api/events/manual/${id}`);
     return response.data;
   } catch (error) {
     console.error("Failed to fetch manual event:", error);
@@ -133,7 +293,9 @@ export const fetchManualEventById = async (id) => {
 
 export const fetchTicketmasterEventById = async (id) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/ticketmaster/${id}`);
+    const response = await axios.get(
+      `${API_BASE}/api/events/ticketmaster/${id}`
+    );
     return response.data;
   } catch (error) {
     console.error("Failed to fetch Ticketmaster event:", error);
@@ -142,18 +304,13 @@ export const fetchTicketmasterEventById = async (id) => {
 };
 
 export const getManualEvents = async () => {
-  const API_BASE_URL = "http://localhost:5000/api/events";
-  const response = await axios.get(`${API_BASE_URL}/manual`);
+  const response = await axios.get(`${API_BASE}/api/events/manual`);
   return response.data;
 };
 
 export const fetchFromTicketmaster = async (externalId) => {
-  const API_KEY = process.env.TICKETMASTER_API_KEY;
-
   try {
-    const res = await axios.get(
-      `https://app.ticketmaster.com/discovery/v2/events/${externalId}.json?apikey=${API_KEY}`
-    );
+    const res = await axios.get(`${API_BASE}/api/ticketmaster/${externalId}`);
     return res.data;
   } catch (error) {
     console.error("Failed to fetch from Ticketmaster", error.message);
