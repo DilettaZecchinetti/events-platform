8c80f7d57f6c8738d48a0a3dd3ac3f31a9ca4f33
DilettaZecchinetti <diletta.zecchinetti@gmail.com>
2025-06-06 21:40:49 +0100

    add client/dist to .gitignore

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,33 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:5000",
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/_redirects",
+          dest: "",
+        },
+      ],
+    }),
+  ],
   server: {
     proxy: {
       "/api": {
